3ccee947ff7473021ddae1481438d13e8e9e2ecf
Idodo <idofarhi@gmail.com>
2018-05-10 06:18:14 +0300

    Additional features and bug fixes Added unique item ID

--- start.py
+++ start.py
@@ -2,47 +2,34 @@ import bs4 as bs
 import requests
 import pymysql
 import pandas as pd
-import multiprocessing as mp
+import concurrent.futures
 from datetime import datetime
 from sqlalchemy import create_engine
 
 
 # TODO: add user-definable options for scope of download
-# TODO: implement threading or multiprocessing
+# TODO: make soup searches more efficient (so don't need soup left & right)
+# TODO: if title starts with brand name, remove it from title.
 
 
 BRANDS_PAGE_LINK = 'https://www.peterglenn.com/brands'
-NUM_PROCESSES = 30
+NUM_CONCURRENT_PROCESSES = 20
+STARTING_BRAND_NUM = 112 # Use ":" to start at first brand
+NUM_BRANDS_TO_SCRAP = 113 # Use ":" to scrap all brands
 
 
 def main():
-    """ Main Function """
+    """
+    Main Function
+    1. Runs site crawl
+    2. Saves crawled data to the database
+    """
 
+    new_products_inventory_df = run_site_crawl(BRANDS_PAGE_LINK)
 
-    # Multiprocessing with queue
-    # Initiate multiprocessing object
-    # pool = multiprocessing.Pool()
-    # output = mp.Queue()
-    # processes = []
-    # for i in range(NUM_PROCESSES):
-    #     processes.append(mp.Process(target = get_item_links_for_brand, args=(brands_link_string_list[i], True, output)))
-    #     sleep(0.5)
-    #     print('process number: ',i, ' starting')
-    #     processes[i].start()
-    #
-    #
-    # for p in processes:
-    #     print('process number: ',p, ' joining')
-    #     p.join()
-    #
-    # results = [len(output.get()) for p in processes]
-    # num_items = sum(results)
-    # print(results,num_items)
+    save_data_to_database(new_products_inventory_df)
 
-    new_items_inventory_df = run_site_crawl(BRANDS_PAGE_LINK)
-
-    save_data_to_database(new_items_inventory_df)
-
+    return
 
 
 """ ****************************************************************** """
@@ -50,9 +37,9 @@ def main():
 """ ****************************************************************** """
 
 
-def get_item_ids(items):
+def get_product_ids(products):
     """********** run mysql command **********"""
-    query = """ SELECT item_id, title FROM items WHERE title in ('{}'); """.format(items)
+    query = """ SELECT product_id, title FROM products WHERE title in ('{}'); """.format(products)
 
     try:
         conn = pymysql.connect(host='localhost', port=3306, user='root', passwd='idofarhi', db='scraping')
@@ -72,30 +59,29 @@ def get_item_ids(items):
 def write_to_database(df,db_name):
     """********** run mysql command **********"""
 
-
     try:
-        if db_name == 'items':
-            df = df[['category', 'brand', 'title', 'msrp']]
+        if db_name == 'products':
+            df = df[['product_id', 'category', 'brand', 'title', 'msrp']]
             conn = create_engine('mysql+pymysql://root:idofarhi@localhost:3306/scraping', echo=False)
-            df.to_sql(name='items', con=conn, index=False, if_exists='append')
+            df.to_sql(name='products', con=conn, index=False, if_exists='append')
 
         elif db_name == 'inventory':
-            df = df[['date_time', 'current_price', 'sizes', 'colors', 'item_id']]
+            df = df[['date_time', 'current_price', 'sizes', 'colors', 'product_id']]
             conn = create_engine('mysql+pymysql://root:idofarhi@localhost:3306/scraping', echo=False)
             df.to_sql(name='inventory', con=conn, index=False, if_exists='append')
 
         else:
-            print('Can only write to "items" or "inventory" tables, you asked to write to: '+db_name)
+            print('Can only write to "products" or "inventory" tables, you asked to write to: '+db_name)
 
     except:
-        print("Error: unable to run mysql command")
+        print("Error: unable to run mysql command. ERROR 1")
 
 
 def get_sql_table(table_name):
     """********** run mysql command **********"""
 
-    if table_name != ('items' or 'inventory'):
-        print('Can only fetch tables "items" and "inventory", you asked for {}'.format(table_name))
+    if table_name != ('products' or 'inventory'):
+        print('Can only fetch tables "products" and "inventory", you asked for {}'.format(table_name))
         return []
 
     try:
@@ -104,29 +90,29 @@ def get_sql_table(table_name):
 
     except:
         # TODO: Output actual error message if an exception occurs
-        # TODO: Catch case of empty table
-        print("Error: unable to get sql table")
+        # TODO: Catch case of empty table separately
+        print("Error: unable to get sql table. ERROR 2")
         return []
 
     return query_result
 
 
-def save_data_to_database(new_items_inventory_df):
+def save_data_to_database(new_products_inventory_df):
     """ Runs the process of saving all the new data to the database tables """
 
-    # Getting the current items table to compare with the new items:
-    items_df = get_sql_table('items')
-    # Find differences between df of new items and items pulled from items table:
-    df_to_items_db = items_df.merge(new_items_inventory_df, on=['category', 'brand', 'title', 'msrp'],
+    # Getting the current products table to compare with the new products:
+    products_df = get_sql_table('products')
+    # Find differences between df of new products and products pulled from products table:
+    df_to_products_db = products_df.merge(new_products_inventory_df, on=['product_id', 'category', 'brand', 'title', 'msrp'],
                                     how='outer', indicator=True).query("_merge == 'right_only'").drop('_merge', 1)
-    # Write new items (if any) to items database:
-    write_to_database(df_to_items_db, 'items')
+    # Write new products (if any) to products database:
+    write_to_database(df_to_products_db, 'products')
 
-    # Get item id's for collected items (need for reference in inventory db):
-    item_ids = get_item_ids(
-        "','".join(list(pd.Series(new_items_inventory_df['title']).apply(lambda x: x.replace("'", "''")))))
-    # Combime pulled id's with collected items:
-    df_to_inventory_db = new_items_inventory_df.merge(item_ids, on='title', how='outer')
+    # Get product id's for collected products (need for reference in inventory db):
+    product_ids = get_product_ids(
+        "','".join(list(pd.Series(new_products_inventory_df['title']).apply(lambda x: x.replace("'", "''")))))
+    # Combime pulled id's with collected products:
+    df_to_inventory_db = new_products_inventory_df.merge(product_ids, on='product_id', how='outer')
     write_to_database(df_to_inventory_db, 'inventory')
 
     return
@@ -139,38 +125,46 @@ def save_data_to_database(new_items_inventory_df):
 
 def run_site_crawl(brands_page_link):
     """ Runs the step by step main process for crawling the peter glenn website """
+
     # Step 1: get all the links for different brands from brands page
     brands_link_string_list = get_brands_page_links(brands_page_link)
 
-    # Step 2: iterate through all brands and get links for full item list:
-    items_details_list = []
-    for brand in brands_link_string_list[:10]:
+    products_details_list = []
 
-        item_links = get_item_links_for_brand(brand)
-        # Step 3: get the details for all items in the brand's link list:
-        brand_items_details_list = get_details_from_brand_links_list(item_links, brand[1])
-        items_details_list += brand_items_details_list
+    def run_through_brands_links(brand):
+
+        # Step 2: iterate through all brands and get a list of links for all their products:
+        product_links = get_product_links_for_brand(brand)
+        # Step 3: get the details for all products in the brand's link list:
+        print('\033[93m' + 'Starting crawl for brand: ' + brand[1]+ '\033[0m')
+        brand_products_details_list = get_details_from_brand_links_list(product_links, brand[1])
+
+        nonlocal products_details_list
+        print('\033[92m'+ 'Finished collecting for brand: '+brand[1]+'\033[0m')
+        products_details_list += brand_products_details_list
+        return
+
+    with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_CONCURRENT_PROCESSES) as executer:
+        executer.map(run_through_brands_links, brands_link_string_list[STARTING_BRAND_NUM:NUM_BRANDS_TO_SCRAP])
+
+    # Putting all the collected brands' products in a dataframe:
+    new_products_inventory_df = pd.DataFrame(products_details_list, columns=['date_time', 'category', 'brand', 'title', 'msrp',
+                                                                       'current_price', 'sizes', 'colors', 'product_id'])
+    return new_products_inventory_df
 
 
-    # Putting all the collected brands' items in a dataframe:
-    new_items_inventory_df = pd.DataFrame(items_details_list, columns=['date_time', 'category', 'brand', 'title', 'msrp',
-                                                                       'current_price', 'sizes', 'colors'])
-    return new_items_inventory_df
-
-
-def get_item_info(link):
-    """ Returns item category, title, msrp, current price, sizes and colors"""
+def get_product_info(link):
+    """ Returns product_id, category, title, msrp, current price, sizes and colors"""
     soup, left_soup, right_soup = divide_page_sections(link)
     category = get_category(soup)
     title, msrp, current_price = get_title_msrp_price(left_soup, right_soup)
-    print(category, title)
     sizes = get_sizes(right_soup)
     colors = get_colors(right_soup)
-    print(msrp, current_price)
-    print(sizes)
-    print(colors)
+    product_id = soup.select_one("span[itemprop*=productID]").text
+    # print(sizes, colors)
+    print('productID:', product_id, '| category:', category, '| title:', title, '| msrp:', msrp, '| current price:', current_price)
 
-    return category, title, msrp, current_price, sizes, colors
+    return category, title, msrp, current_price, sizes, colors, product_id
 
 
 def divide_page_sections(link):
@@ -197,9 +191,9 @@ def get_category(soup):
 
 def get_title_msrp_price(left_soup, right_soup):
     # TODO: Currently only takes lowest price. Add ability to pull price by color/size
-    """ Get item title, MSRP and price """
-    # we split the price at a space and take the first item so that we get
-    # the lowest price in case the item has a range of prices
+    """ Get product title, MSRP and price """
+    # we split the price at a space and take the first product so that we get
+    # the lowest price in case the product has a range of prices
     current_price = float(right_soup.find('span', itemprop='price').text.split(' ', 1)[0][1:])
     title = left_soup.find('h1', class_="title").text
     try:
@@ -217,8 +211,8 @@ def get_sizes(right_soup):
     sizes = []
     try:
         size_soup = right_soup.find_all('div', class_='choice')
-        for item in size_soup:
-            sizes.append(item.find(class_='size_text').text)
+        for product in size_soup:
+            sizes.append(product.find(class_='size_text').text)
         if sizes == []:
             sizes = ['O/S']
     except AttributeError:
@@ -232,8 +226,8 @@ def get_colors(right_soup):
     colors = []
     try:
         color_soup = right_soup.find('div', class_='color').find_all('img')
-        for item in color_soup:
-            colors.append(item.get('id'))
+        for product in color_soup:
+            colors.append(product.get('id'))
     except AttributeError:
         # No color specified.
         colors = ['N/A']
@@ -274,8 +268,8 @@ def get_brands_page_links(brands_page_link):
     return brands_link_string_list
 
 
-def get_item_links_for_brand(brand_tuple, multiprocessing = False, queue = None):
-    """ Receives a brand, returns a list of links of all items by that brand"""
+def get_product_links_for_brand(brand_tuple, multiprocessing = False, queue = None):
+    """ Receives a brand, returns a list of links of all products by that brand"""
 
     brand_page_link = brand_tuple[0]
 
@@ -305,11 +299,11 @@ def get_item_links_for_brand(brand_tuple, multiprocessing = False, queue = None)
 
 
 def get_details_from_brand_links_list(list_of_links, brand):
-    """ Gets the details for all items in the list of links. """
+    """ Gets the details for all products in the list of links. """
     lst = []
     for link in list_of_links:
-        category, title, msrp, current_price, sizes, colors = get_item_info(link)
-        lst.append([datetime.now().strftime("%y-%m-%d-%H-%M"), category, brand, title, msrp, current_price, sizes, colors])
+        category, title, msrp, current_price, sizes, colors, product_id = get_product_info(link)
+        lst.append([datetime.now().strftime("%y-%m-%d-%H-%M"), category, brand, title, msrp, current_price, sizes, colors, product_id])
 
     return lst
 
